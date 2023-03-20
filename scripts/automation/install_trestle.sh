cf1551418bf2450432b4c2b82250e48cbd6cdbf5
Ekaterina Nikonova <enikonovad@gmail.com>
2023-03-20 13:26:35 +1100

    feat: Update ssp
    Signed-off-by: Ekaterina Nikonova <enikonovad@gmail.com>

--- scripts/automation/install_trestle.sh
+++ scripts/automation/install_trestle.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python3 -m pip install --upgrade pip setuptools
-python3 -m pip install compliance-trestle==2.0.0
+#python3 -m pip install compliance-trestle==2.0.0
+python3 -m pip install git+https://github.com/IBM/compliance-trestle@fix/ssp_profile
 python3 -m pip install python-semantic-release==7.31.4
